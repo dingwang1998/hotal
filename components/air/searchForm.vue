0a5e2486833a44437c34f3e14d9ad5f79115a633
bernie <bernieweidongz@outlook.com>
2020-04-20 22:02:19 +0800

    开关思想k表单验证

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -22,7 +22,12 @@
           </span>
         </el-row>
 
-        <el-form class="search-form-content" :model="form" ref="form" label-width="80px">
+        <el-form
+          class="search-form-content"
+          :model="form"
+          ref="form"
+          label-width="80px"
+        >
           <!-- 出发城市输入框 -->
           <el-form-item label="出发城市">
             <!-- fetch-suggestions 返回输入建议的方法 根据输入的关键字，发请求，返回的数据会出现在下拉列表中 -->
@@ -53,7 +58,8 @@
           <el-form-item label="出发时间">
             <!-- change 用户确认选择日期时触发 -->
             <el-date-picker
-            v-model="value1"
+              v-model="form.departDate"
+              value-format="yyyy-MM-dd"
               type="date"
               placeholder="请选择日期"
               style="width: 100%;"
@@ -123,24 +129,22 @@ export default {
       // 表单数据
       form: {
         departCity: '', //出发城市
-        departCode:'',//出发城市代码
+        departCode: '', //出发城市代码
         destCity: '', //到达城市
-        destCode:'',//到达城市代码
-        departDate: '', //出发日期
+        destCode: '', //到达城市代码
+        departDate: '' //出发日期
       },
       // 失去焦点时候默认的数据
-      departCity:"",//出发
-      destCity:"",//目的地
+      departCity: [], //出发
+      destCity: [], //目的地
       // 选择可选日期区域 pickerOptions对象
-      pickerOptions:{
+      pickerOptions: {
         // 这里的time是自动传入的
         disabledDate(time) {
-            // 这里的<号代表之前的天数不可选，-3600 * 1000 * 24是除掉当天不可选之外的，都可以被选中
-            return time.getTime() < Date.now()- 3600 * 1000 * 24;
-          },
+          // 这里的<号代表之前的天数不可选，-3600 * 1000 * 24是除掉当天不可选之外的，都可以被选中
+          return time.getTime() < Date.now() - 3600 * 1000 * 24
+        }
       },
-      // 选中的时间
-      value1:""
     }
   },
   methods: {
@@ -156,57 +160,61 @@ export default {
     // 出发城市输入框获得焦点时触发
     // value 是选中的值，cb是回调函数，接收要展示的列表
     async queryDepartSearch(value, cb) {
-      if(!value)return;
-      const res = await this.$axios.get('/airs/city', {params: { name: value }})
-      const { data } = res.data;
-      // console.log(data);
-      const newData=data.map(v=>{
-        v.value=v.name.replace('市','')
-        return v;
+      if (!value) return
+      const res = await this.$axios.get('/airs/city', {
+        params: { name: value }
       })
-      this.departCity=newData;
-      cb(newData);
+      const { data } = res.data
+      // console.log(data);
+      const newData = data.map(v => {
+        v.value = v.name.replace('市', '')
+        return v
+      })
+      this.departCity = newData
+      cb(newData)
     },
     // 目标城市输入框获得焦点时触发
     // value 是选中的值，cb是回调函数,cb里面接受一个数组，数组用对象形式存储信息，接收要展示的列表
     async queryDestSearch(value, cb) {
-      if(!value) return;
-      const res = await this.$axios.get('/airs/city', {params: { name: value }})
-      const { data } = res.data;
-      console.log(data);
-      const newData=data.map(v=>{
-        v.value=v.name.replace('市','')
-        return v;
+      if (!value) return
+      const res = await this.$axios.get('/airs/city', {
+        params: { name: value }
       })
-      this.destCity=newData;
-      cb(newData);
+      const { data } = res.data
+      console.log(data)
+      const newData = data.map(v => {
+        v.value = v.name.replace('市', '')
+        return v
+      })
+      this.destCity = newData
+      cb(newData)
     },
     // 出发城市失去焦点
-    handleDeparBlur(){
-      if(this.departCity.length>0){
+    handleDeparBlur() {
+      if (this.departCity.length > 0) {
         // 将出发城市表单中的默认值设置为返回条件的第一个城市
-        this.form.departCity=this.departCity[0].value;
+        this.form.departCity = this.departCity[0].value
         // 根据API，需要存入出发城市code
-        this.form.departCode=this.departCity[0].sort;
+        this.form.departCode = this.departCity[0].sort
       }
     },
     // 到达城市失去焦点
-    handledestBlur(){
-      if(this.destCity.length>0){
-        this.form.destCity=this.destCity[0].value;
+    handledestBlur() {
+      if (this.destCity.length > 0) {
+        this.form.destCity = this.destCity[0].value
         // 根据API，需要存入到达城市code
-        this.form.destCode=this.destCity[0].sort;
+        this.form.destCode = this.destCity[0].sort
       }
     },
 
     // 出发城市下拉选择时触发
     handleDepartSelect(item) {
-      this.form.departCode=item.sort;
+      this.form.departCode = item.sort
     },
 
     // 目标城市下拉选择时触发
     handleDestSelect(item) {
-      this.form.destCode=item.sort
+      this.form.destCode = item.sort
     },
 
     // 确认选择日期时触发
@@ -216,7 +224,28 @@ export default {
     handleReverse() {},
 
     // 提交表单时触发
-    handleSubmit() {}
+    handleSubmit() {
+      // 创建表单验证的信息数组
+      const arr = [
+        { value: this.form.departCity === '', message: '请输入出发城市' },
+        { value: this.form.destCity === '', message: '请输入目的地' },
+        { value: this.form.departDate === '', message: '请输入出发日期' }
+      ];
+      // 开关思想
+      let flag = true;
+      // 遍历数组中的value,只要有一个验证没通过，就阻止继续
+      arr.forEach(v=>{
+        if(!flag)return;
+        // 判断每一项的value是否为真
+        if(v.value==true){
+          // 为真代表数值为空 就提醒用户要输入东西 然后把flag设置为false
+          this.$message.warning(v.message);
+          flag=false;
+        }
+      });
+      // 有一项不通过，就会停止执行，如果flag为true时候才会继续执行
+      if(!flag) return;
+    }
   },
   mounted() {}
 }
